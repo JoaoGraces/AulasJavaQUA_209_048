f4fe936108e16c36f1a6a68751765949242c9cbe
joaograces <jvbernardes420@gmail.com>
2022-07-18 16:42:15 -0300

    Atividade variaveis

---
+++ Algoritimo-01Exercicio/src/aulas/PrimeiroExercicio.java
@@ -0,0 +1,38 @@
+package aulas;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+		
+		int numero1 = 10;
+		double numero2 = 69.420;
+		
+		char sexo = 'M';
+		String carro = "Monza";
+		
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+		System.out.println(sexo);
+		System.out.println(carro);
+		
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		
+		/*Concatenação p */
+		
+		int idade = 18;
+		String nome = "João";
+		double dinheiro = 00.00;
+		
+		System.out.println(nome + " tem " + idade + " anos, e tem " + dinheiro + " reais");
+		
+		
+		
+
+	}
+
+}
