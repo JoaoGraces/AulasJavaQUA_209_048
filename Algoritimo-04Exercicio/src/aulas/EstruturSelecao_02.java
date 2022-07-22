8ea8cdfd5a77af8b2022d564fbff939897874534
joaograces <jvbernardes420@gmail.com>
2022-07-22 15:01:33 -0300

    Estrutura condicional

---
+++ Algoritimo-04Exercicio/src/aulas/EstruturSelecao_02.java
@@ -0,0 +1,36 @@
+package aulas;
+
+import java.util.Scanner;
+
+public class EstruturSelecao_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = 0, n2 = 0, n3 = 0;
+		
+		System.out.println("Descubra qual dos 3 números é maior");
+		
+		System.out.print("Digite o primeiro número: ");
+		n1 = sc.nextInt();
+		
+		System.out.print("Digite o segundo número: ");
+		n2 = sc.nextInt();
+		
+		System.out.println("Digite o terceiro número: ");
+		n3 = sc.nextInt();
+		
+		if(n1 > n2 && n1 > n3) {
+			System.out.printf("%d é o maior número!", n1);
+		}
+		else if (n2 > n3){
+			System.out.printf("%d é o maior número!", n2);
+		}
+		else {
+			System.out.printf("%d é o maior número", n3);
+		}
+	}
+
+}
---
+++ Algoritimo-04Exercicio/src/aulas/EstruturaSelecao_01.java
@@ -0,0 +1,30 @@
+package aulas;
+
+import java.util.Scanner;
+
+public class EstruturaSelecao_01 {
+
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = 0, n2 = 0;
+		
+		System.out.println("Digite dois valores inteiros e descubra qual deles é o maior!");
+		
+		System.out.println("Escreva o primeiro numero: ");
+		n1 = sc.nextInt();
+		
+		System.out.println("Escreva o primeiro numero: ");
+		n2 = sc.nextInt();
+		
+		if (n1 > n2) {
+			System.out.printf("%d o maior número!", n1);
+		}
+		else{
+			System.out.printf("%d o mior número!", n2);
+		}
+		
+		
+		
+	}
+}
